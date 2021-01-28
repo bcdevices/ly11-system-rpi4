4bee2c184b72724ef92ea8d06cbb25d535ab150c
Eric Rauer <github@rauer.dev>
2021-01-28 12:14:31 -0800

    Update fit image handling

--- uboot/boot.cmd
+++ uboot/boot.cmd
@@ -1,4 +1,5 @@
 boot_fit=bootm ${fit_addr}
+fdt addr ${fdt_addr} && fdt get value bootargs /chosen bootargs
 fit_addr=0x1F000000
-load_fit=fatload mmc 0:1 ${fit_addr} image.fit
-mmcboot=run load_fit; run set_bootargs_tty set_bootargs_mmc set_common_args; run boot_fit
+fatload mmc 0:1 ${fit_addr} image.fit
+run boot_fit
