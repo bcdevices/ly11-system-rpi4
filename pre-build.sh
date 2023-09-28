7c78356933ec936fc648de45ad615842a10b7005
Eric Rauer <eric@rauer.dev>
2023-09-28 14:39:53 -0700

    Set packages from bootstrap script

--- pre-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-local PACKAGES_VERSION=`cat "$NERVES_DEFCONFIG_DIR/PACKAGES-VERSION"`
-echo $PACKAGES_VERSION
-wget "https://github.com/bcdevices/ly10-buildroot-packages/releases/download/v$PACKAGES_VERSION/buildroot-packages-$PACKAGES_VERSION.tar.gz"
-tar xzf "buildroot-packages-$PACKAGES_VERSION.tar.gz"
-rm "buildroot-packages-$PACKAGES_VERSION.tar.gz"
-ln -sf package-$PACKAGES_VERSION package
