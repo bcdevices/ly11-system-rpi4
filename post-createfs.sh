2f34cc3f8338fdb58ce8d52d96b8d8ac1fd7df73
Eric Rauer <eric@rauer.dev>
2023-09-29 14:57:57 -0700

    Move uboot config to pltos

--- post-createfs.sh
+++ post-createfs.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-mkimage -A arm64 -O linux -T script -C none -a 0 -e 0 -n 'u-boot script' -d "${NERVES_DEFCONFIG_DIR}"/boot.cmd "${BINARIES_DIR}"/boot.scr
-
 FWUP_CONFIG=$NERVES_DEFCONFIG_DIR/fwup.conf
 
 
