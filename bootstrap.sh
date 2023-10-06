e32578ad51d2de44c1109150f1453adfacf10ecc
Eric Rauer <eric@rauer.dev>
2023-10-06 13:16:04 -0700

    Clean up bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-export SCRIPT_DIR=`dirname "$0"`
+export SCRIPT_NAME=`realpath "$0"`
+export SCRIPT_DIR=`dirname "$SCRIPT_NAME"`
 
-export PACKAGES_VERSION=`cat "$SCRIPT_DIR/PACKAGES-VERSION"`
-curl -o "https://github.com/bcdevices/ly10-buildroot-packages/releases/download/v$PACKAGES_VERSION/buildroot-packages-$PACKAGES_VERSION.tar.gz" 
-tar xzf "$SCRIPT_DIR/buildroot-packages-$PACKAGES_VERSION.tar.gz"
-rm "$SCRIPT_DIR/buildroot-packages-$PACKAGES_VERSION.tar.gz"
-ln -sf "$SCRIPT_DIR/package-$PACKAGES_VERSION" "$SCRIPT_DIR/package"
+make -C "$SCRIPT_DIR" sync-packages
+
