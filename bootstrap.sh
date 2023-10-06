631c41b80788d82e65379bacae501f47b87a6f2e
Eric Rauer <eric@rauer.dev>
2023-10-06 12:59:48 -0700

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,7 @@
 export SCRIPT_DIR=`dirname "$0"`
 
 export PACKAGES_VERSION=`cat "$SCRIPT_DIR/PACKAGES-VERSION"`
-wget "https://github.com/bcdevices/ly10-buildroot-packages/releases/download/v$PACKAGES_VERSION/buildroot-packages-$PACKAGES_VERSION.tar.gz" 
+curl -o "https://github.com/bcdevices/ly10-buildroot-packages/releases/download/v$PACKAGES_VERSION/buildroot-packages-$PACKAGES_VERSION.tar.gz" 
 tar xzf "$SCRIPT_DIR/buildroot-packages-$PACKAGES_VERSION.tar.gz"
 rm "$SCRIPT_DIR/buildroot-packages-$PACKAGES_VERSION.tar.gz"
 ln -sf "$SCRIPT_DIR/package-$PACKAGES_VERSION" "$SCRIPT_DIR/package"
